5cc8d13402e650179bcc3afad65eb28dbb5389b5
elvis <gicharuelvis@gmail.com>
2022-09-27 10:10:21 +0300

    commit tree 1841f4592fd5dc060e62c90bbd47550f61d8c5fc

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const superbowlWin = record =>{
+    for (const bowl in superbowlWin){
+        if(record.result ==='W'){
+            return result.year
+        }
+        else{
+            return undefined;
+        }
+    }
+}
